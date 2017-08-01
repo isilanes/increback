bd92d684188fae3ef5d29859414b46c167eef38b
Iñaki Silanes <isilanes@gmail.com>
2017-08-01 10:29:42 +0200

    more tests

--- test/test_logworks.py
+++ test/test_logworks.py
@@ -14,7 +14,14 @@ class TestLogger(unittest.TestCase):
 
     # Setup and teardown:
     def setUp(self):
-        pass
+        self.texts = ["something", "yadda yadda", "", None]
+        self.colors = ["name", "info", "warning", "error"]
+        self.conf_colors = {
+            "name": 1,
+            "info": 2,
+            "warning": 3,
+            "error": 4,
+        }
 
     def tearDown(self):
         pass
@@ -46,9 +53,8 @@ class TestLogger(unittest.TestCase):
         logger = logworks.Logger()
         logger.logger.info = mock.Mock()
         logger.with_info_color = mock.Mock()
-        texts = [ "something", "yadda yadda", "", None]
 
-        for text in texts:
+        for text in self.texts:
             # Prepare:
             logger.logger.info.reset_mock()
             logger.with_info_color.reset_mock()
@@ -64,14 +70,13 @@ class TestLogger(unittest.TestCase):
     def test_warning(self):
         # Prepare:
         logger = logworks.Logger()
-        logger.logger.info = mock.Mock()
-        logger.with_info_color = mock.Mock()
-        texts = [ "something", "yadda yadda", "", None]
+        logger.logger.warning = mock.Mock()
+        logger.with_warning_color = mock.Mock()
 
-        for text in texts:
+        for text in self.texts:
             # Prepare:
             logger.logger.warning.reset_mock()
-            logger.with_info_color.reset_mock()
+            logger.with_warning_color.reset_mock()
 
             # Run:
             logger.warning(text)
@@ -79,5 +84,94 @@ class TestLogger(unittest.TestCase):
             # Assert:
             logger.logger.warning.assert_called_once()
             self.assertIn(text, logger.logger.warning.call_args[0])
-            logger.with_info_color.assert_called_once()
+            logger.with_warning_color.assert_called_once()
+
+    def test_error(self):
+        # Prepare:
+        logger = logworks.Logger()
+        logger.logger.error = mock.Mock()
+        logger.with_error_color = mock.Mock()
+
+        for text in self.texts:
+            # Prepare:
+            logger.logger.error.reset_mock()
+            logger.with_error_color.reset_mock()
+
+            # Run:
+            logger.error(text)
+
+            # Assert:
+            logger.logger.error.assert_called_once()
+            self.assertIn(text, logger.logger.error.call_args[0])
+            logger.with_error_color.assert_called_once()
+
+
+    # Text colorizers:
+    def test_with_name_color(self):
+        # Prepare:
+        logger = logworks.Logger()
+
+        # Run:
+        for text in self.texts:
+            ret = logger.with_name_color(text)
+
+            # Assert:
+            self.assertIn(str(text), str(ret))
+
+    def test_with_info_color(self):
+        # Prepare:
+        logger = logworks.Logger()
+
+        # Run:
+        for text in self.texts:
+            ret = logger.with_info_color(text)
+
+            # Assert:
+            self.assertIn(str(text), str(ret))
+
+    def test_with_warning_color(self):
+        # Prepare:
+        logger = logworks.Logger()
+
+        # Run:
+        for text in self.texts:
+            ret = logger.with_warning_color(text)
+
+            # Assert:
+            self.assertIn(str(text), str(ret))
+
+    def test_with_error_color(self):
+        # Prepare:
+        logger = logworks.Logger()
+
+        # Run:
+        for text in self.texts:
+            ret = logger.with_error_color(text)
+
+            # Assert:
+            self.assertIn(str(text), str(ret))
+
+    def test_color_for_can(self):
+        # Prepare:
+        logger = logworks.Logger()
+        logger.conf = {"colors": self.conf_colors}
+
+        # Run:
+        for which in self.colors:
+            ret = logger._color_for(which)
+
+            # Assert:
+            self.assertEqual(ret, self.conf_colors[which])
+
+    def test_color_for_cant(self):
+        # Prepare:
+        logger = logworks.Logger()
+        logger.conf = {}
+
+        # Run:
+        for which in self.colors:
+            ret = logger._color_for(which)
+
+            # Assert:
+            self.assertEqual(ret, 0)
 
